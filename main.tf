1dd648d48cc9f04420220ce7b4d63c7b3b028686
vivek tripathi <montitripathi12345@gmail.com>
2025-07-23 08:40:45 +0530

    in the git

--- main.tf
+++ main.tf
@@ -15,3 +15,11 @@ resource "azurerm_resource_group" "rg" {
   name     = "vivek-rg"
   location = "westus"
 }
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "vivekstorageac"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
