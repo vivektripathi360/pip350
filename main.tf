233409c559c9889dd3875c7d24a4047807c417cc
vivek tripathi <montitripathi12345@gmail.com>
2025-07-23 08:55:27 +0530

    in the git

--- main.tf
+++ main.tf
@@ -23,3 +23,9 @@ resource "azurerm_storage_account" "storage_account" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_storage_container" "storage_container" {
+  name                  = "vivekcontainer"
+  storage_account_name  = "vivekstorageac"
+  container_access_type = "private"
+}
